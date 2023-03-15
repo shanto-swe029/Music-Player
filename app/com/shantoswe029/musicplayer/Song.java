09100d1a8e72e1b517c624615042fae398a7ff35
Ariful Islam Shanto <ariful.shanto786@gmail.com>
2023-03-16 02:02:18 +0600

    Added Song.java file

---
+++ app/com/shantoswe029/musicplayer/Song.java
@@ -0,0 +1,16 @@
+package com.shantoswe029.musicplayer;
+
+public class Song {
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long id, String title, String artist) {
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+    }
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+}
