c565969d1c03aa8695458c5ec0c7de9524908846
Ariful Islam Shanto <ariful.shanto786@gmail.com>
2023-03-16 02:04:02 +0600

    Add files via upload

---
+++ app/com/shantoswe029/musicplayer/MusicController.java
@@ -0,0 +1,11 @@
+package com.shantoswe029.musicplayer;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+public class MusicController extends MediaController {
+    public MusicController(Context c){
+        super(c);
+    }
+    public void hide(){}
+}
